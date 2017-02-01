8efd9925de3462c6ccc50da0f3a383a4db08a495
Eddie Quan <therealeddiequan@gmail.com>
2017-01-31 19:17:33 -0500

    authentication on client-side

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import Auth from './lib/auth';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    console.log("Hello");
+    this.state = { loggedIn: Auth.loggedIn() };
+  }
+
+  setStateOnAuth = (loggedIn) => {
+    return this.setState({loggedIn});
+  }
+
+  componentDidMount = () => {
+    var _this = this;
+    var instance = axios.create({baseURL: 'localhost:8080'});
+    this.serverRequest =
+      axios.post('http://localhost:8080/authenticate', 
+          {email: "eddiequan@hotmail.com", password:"password"}).then((response) => console.log(response.data.auth_token)).catch((error) => console.log(error));
+  }
+
+  componentWillMount = () => {
+    return Auth.onChange = this.setStateOnAuth;
+  }
+
+  handleClick = (e) => {
+    Auth.login("eddiequan@hotmail.com", "password", this.setStateOnAuth);
+    console.log('The link was clicked.');
+    console.log(this.state.loggedIn);
+  }
+
+  render = () => {
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +43,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleClick}>Hello?</button>
       </div>
     );
   }
