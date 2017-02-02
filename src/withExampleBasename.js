b0a015ebe0a1cb9903ecaafa0aab3d86ed7c6ef4
Eddie Quan <therealeddiequan@gmail.com>
2017-02-02 13:05:23 -0500

    todo comment to remove basename if unnecessary

--- src/withExampleBasename.js
+++ src/withExampleBasename.js
@@ -2,6 +2,8 @@ import useBasename from 'history/lib/useBasename'
 
 // This helper is for setting basename on examples with minimal boilerplate. In
 // an actual application, you would build a custom history to set basename.
+//
+// TODO: remove if possible.
 export default function withExampleBasename(history, dirname) {
   return useBasename(() => history)({ basename: `/` })
 }
